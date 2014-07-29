0ecbc169c65db474f97c1a09bdfda9f93cf8643b
Brian Dilts <brianedilts@gmail.com>
2014-07-29 17:39:07 -0600

    finished

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-	res.send('<h1>Hello Boulder</h1>');
+	res.send('<h1>Hi there</h1>');
 });
 
 var server = app.listen(process.env.PORT || 9624, function() {
